dd0b1a1ed2fe62014ddb1efeb39753a771b693f8
Eric Chiang <echiang777@yahoo.com>
2017-06-23 12:05:27 -0500

    Initial Commit

---
+++ git-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git-demo
+//
+//  Created by Eric Chiang on 6/23/17.
+//  Copyright © 2017 Eric Chiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
